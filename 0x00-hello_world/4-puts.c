6f026c862f976f0348036e41b50c4a7566b314a4
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-06 00:07:18 +0000

    Adds exercise 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*This is a short program to out put a short string*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
