f2b1d268fa010c352c61640df7101cca7f7218b9
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-06 19:38:58 +0000

    Adds exercise 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-/*This is a short program to print out the alphabet */
+/**
+ * main - entry point
+ *
+ * This is a short program to print out
+ * every lowercase letter in the alphabet
+ *
+ * Return: Always 0 (Succes)
+ */
+
 int main(void)
 {
+	char alpha[27] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	char ch;
 
+	for (i = 0; i < 27; i++)
+	{
+		ch = alpha[i];
+		putchar(ch);
+	}
 
+	putchar('\n');
+
+	return (0);
 }
