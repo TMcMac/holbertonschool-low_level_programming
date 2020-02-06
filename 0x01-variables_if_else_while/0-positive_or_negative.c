26b6f2013d207d30e83e98c1221c17e464839a66
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-06 18:26:49 +0000

    Removes period from first printf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,19 +6,20 @@
   * This is a short program to assign a random number to n
   * to n and then output if n is positive or negative.
   */
+
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-if (n > 0)
-	printf("%d is positive.\n", n);
-else if (n == 0)
-	printf("%d is zero\n", n);
-else
-	printf("%d is negative\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
-return (0);
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/*This is a short program to print out the alphabet */
+int main(void)
+{
+
+
+}
