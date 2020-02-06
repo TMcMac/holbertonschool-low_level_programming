3698d52d0c1d0aaddbf0781fae82d20cc7f4c207
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-06 16:51:06 +0000

    Adds the first completed task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+ /*
+  * This is a short program to assign a random number to n
+  * to n and then output if n is positive or negative.
+  */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+	printf("%d is positive.\n", n);
+else if (n == 0)
+	printf("%d is zero\n", n);
+else
+	printf("%d is negative\n", n);
+
+return (0);
+}
