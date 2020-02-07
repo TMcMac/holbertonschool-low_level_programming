52c5f21ab442f5bf92cea02624bb5079acaffbde
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-07 04:30:50 +0000

    I documented my code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,7 +6,7 @@
  * The goal of this program is to print all
  * combinations of two digits in ascending
  * order with no doubles and no repeated
- * combination. is no 00 or 10 after 01.
+ * combination. ie no 00 or 10 after 01.
  *
  * Return: Always 0 (Success)
  */
@@ -24,23 +24,25 @@ int main(void)
 		for (ones += jump; ones < 10; ones++)
 		{
 			if (tens == ones)
-				;
+				; /* Skip all doubles 00, 11, 22, etc  */
 			else
 			{
 				putchar(tens + '0');
 				putchar(ones + '0');
 				if (tens == 8 && ones == 9)
-					;
+					; /* Written this way so there is no comma space after final digit pair */
 				else
 				{
-					putchar(',');
+					putchar(','); /* add a comma and a space after each digit pair  */
 					putchar(' ');
 				}
 			}
 		}
-		ones = 0;
-		jump += 1;
-		tens++;
+		ones = 0; /* Reset the one's for the next loop */
+		jump += 1; /* This will allow for the jump each
+			    * look to skip repeats like 10 20 30 etc
+			    */
+		tens++; /* This will increment the 10s digit after each for loop */
 	}
 
 
