faac2feeceff4afc7733b9b3a8ad63c3f747a1bb
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-11 03:42:45 +0000

    adds exercise 6 and 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <unistd.h>
+
+/**
+ * print_last_digit - a function that returns
+ * the last digit of a supplied int.
+ *
+ * @n: an int that is passed to the function
+ *
+ * Return: Returns the last digit of an int
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+	{
+		n = (n * -1);
+		last_digit  = (n % 10);
+	}
+	else
+		last_digit = (n % 10);
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
+
+
