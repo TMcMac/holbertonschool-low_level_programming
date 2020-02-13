e5cb325ca838589ce60518e617ec3c0dba70593b
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-13 17:35:19 +0000

    Updates holberton.h and adds exercise 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print numbers 0 to 9 but not
+ * 2 or 4.
+ *
+ *
+ * Return: No return.
+ */
+
+
+void print_most_numbers(void)
+{
+	int i = 0;
+	
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{			
+			_putchar(i + '0');
+			i++;
+		}
+		else
+			i++;
+	}
+	_putchar('\n');
+}
