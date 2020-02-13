00eef20f923b2d68ced02e01a14fa8a9f9b42e06
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-13 18:20:43 +0000

    Corrects max of range to 57

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,12 @@
  *
  * @c: an int signifying a char
  *
- * Return: 1 if digit, 0 is other
+ * Return: 1 if digit, 0 if other
  */
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 58)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
