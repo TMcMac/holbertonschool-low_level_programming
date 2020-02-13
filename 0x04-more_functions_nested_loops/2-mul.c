884c3236ecb17dcab0bf0b0eb37e032f5e0afdd5
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-13 16:29:08 +0000

    Updates holberton.h and add exercise 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - a function to multiple to ints
+ *
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: the value of sum;
+ */
+
+int mul(int a, int b)
+{
+	int sum = (a * b);
+
+	return (sum);
+}
