0c6aac98e741c5415c13b6b8d37b951a183371f3
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-13 04:18:54 +0000

    Updates with betty style and main desc

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - this function sums all the natural
+ * numbers from 0to 1023 that are divisable by
+ * 3 or 5.
+ *
+ * Return: there is no return.
+ */
 int main(void)
 {
 	int x = 0;
 	int sum;
+
 	while (x < 1024)
 	{
 		if (x % 3 == 0 || x % 5 == 0)
