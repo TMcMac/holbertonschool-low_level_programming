cfce7583f1dc1b06d8e8c372413380004523f466
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-13 04:13:48 +0000

    Adds exercise 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 0;
+	int sum;
+
+	while (x < 1024)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			sum += x;
+			x++;
+		}
+		else
+			x++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
