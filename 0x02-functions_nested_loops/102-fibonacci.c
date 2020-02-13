4c63b30db36cdf89ee38f7391507f8e9124ad3cd
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-13 04:45:51 +0000

    Corrects the counter and removes trailing comma

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,19 +8,20 @@
 
 int main(void)
 {
-	long long int fib1 = 1;
-	long long int fib2 = 2;
-	long long int fibnext, i;
+	long int fib1 = 1;
+	long int fib2 = 2;
+	long int fibnext, i;
 
-	for (i = 0; i < 50; i++)
+	for (i = 0; i < 48; i += 2)
 	{
-		printf("%lld, ", fib1);
-		printf("%lld, ", fib2);
+		printf("%ld, ", fib1);
+		printf("%ld, ", fib2);
 		fibnext = (fib1 + fib2);
 		fib1 += fib2;
 		fib2 += fibnext;
 	}
-	printf("%lld", fibnext);
+	printf("%ld, ", fib1);
+	printf("%ld", fib2);
 	printf("\n");
 
 	return (0);
