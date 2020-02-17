b8fbfe96e51c70dc626660144ffb39d19e9d1345
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-17 20:36:03 +0000

    Adds exercise 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - a function that updates the value
+ * of an int to 98 using a pointer
+ *
+ * @n: a pointer passed from main
+ *
+ * Return: there is no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
