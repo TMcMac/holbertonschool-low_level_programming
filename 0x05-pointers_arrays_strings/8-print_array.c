9818c0fe21a99c7d6959c7cb4be3a0be9e396bfa
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-18 21:39:12 +0000

    Reworks the print statement for newline and comma space

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,8 +18,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", a[i]);
-	printf("%d\n", a[i]);
-      
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+		else
+			printf("\n");
+	}      
 }
