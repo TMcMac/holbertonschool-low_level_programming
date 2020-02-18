e84166c89d3fb789e4367db00e730085d001f9f0
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-18 20:15:23 +0000

    adds exercise 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * puts_half - a fuction to print the second half of a string
+ *
+ * @str: a string passed from main.
+ *
+ * Return: there is no return
+ */
+
+void puts_half(char *str)
+{
+	int max;
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	max = i;
+
+	for (i = (i / 2); i <= max; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
