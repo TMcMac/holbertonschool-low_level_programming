67b33af7910183a0e5bd9453d2699d4ca4d72d55
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-20 23:25:06 +0000

    Adds exercise 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+
+int _strlen(char *s);
+
+/**
+ * cap_string - a function to capitalize the first letter of each
+ * word in a string
+ *
+ * @str: a string passed from main
+ *
+ * Return: return the string with capitalizations
+ */
+
+
+
+char *cap_string(char *str)
+{
+	int count = 0;
+	int length = _strlen(str);
+
+	while (count < length)
+	{
+		while (str[count] == '\t' || str[count] == ' ' || str[count] == '!'
+		    || str[count] == '"' || str[count] == '(' || str[count] == ')'
+		    || str[count] == '.' || str[count] == ';' || str[count] == '?'
+		    || str[count] == '{' || str[count] == '}' || str[count] == '\n')
+		{
+			count++;
+			if (str[count] >= 'a' && str[count] <= 'z')
+				str[count] -= 32;
+		}
+		count++;
+	}
+	return (str);
+}
+
+
+/**
+ * _strlen - a function to return the length of a string
+ *
+ * @s: a char pointer passed from main
+ *
+ * Return: returns an int
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		continue;
+
+	return (len);
+}
