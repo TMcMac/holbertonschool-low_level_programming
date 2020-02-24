9a06a365b99eb87d4191c04828e8d7fde6fb81f4
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-24 21:57:42 +0000

    Adds first attempt at project 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <unistd.h>
+
+int _putchar(char c);
+
+/**
+ * print_chessboard - a function to print a chessboard
+ *
+ * @a: a pointer to the start of an array
+ *
+ * Return: no return
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		if (i < 8)
+			_putchar('\n');
+	}
+}
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
