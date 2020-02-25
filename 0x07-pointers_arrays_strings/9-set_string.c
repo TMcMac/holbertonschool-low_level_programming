83e0a9ae3446caf3d3273ddeeda68d6c65cd0b76
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-02-25 01:12:32 +0000

    adds exercise 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_string - fuction to point a double pointer at a pointer
+ *
+ * @t: a pointer
+ * @s: a double pointer
+ *
+ * Return: no return
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
