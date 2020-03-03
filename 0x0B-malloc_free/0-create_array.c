e7f6e00193ce807b0b6161f8fedff288b6015e47
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-03 18:35:47 +0000

    Adds typecast to malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -24,7 +24,7 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		array = malloc(size * sizeof(char));
+		array = (char*) malloc(size * sizeof(char));
 		for (i = 0; i < size; i++)
 			array[i] = c;
 	}
