e8f53ef7ca3314995eb7079eea5a375c001e9419
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-03 19:26:20 +0000

    Fixes an issue with betty style and adds null bite on end of new string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,17 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _strlen(char *s);
-
 /**
- * *_strdup - a function to copy a string to a new location and
+ * _strdup - a function to copy a string to a new location and
  * return a pointer to the new location.
  *
  * @str: an unsigned int fed from main that will be the size of our array
  *
  * Return: NULL or pointer to the filled array.
  */
-
 
 char *_strdup(char *str)
 {
@@ -25,25 +22,25 @@ char *_strdup(char *str)
 		return (NULL);
 	}
 	else
-		new = (char*) malloc(length * sizeof(char));
+		new = (char *)malloc(length * sizeof(char));
 
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 	else
+	{
 		for (i = 0; i < length; i++)
 			new[i] = str[i];
-
+		new[i] = '\0';
+	}
 	return (new);
 
 }
 
  /**
   * _strlen - a function to return the length of a string
-  *
   * @s: a char pointer passed from main
-  *
   * Return: returns an int for the length of s
   */
 
