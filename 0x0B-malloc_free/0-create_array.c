f5bd8b1420e9f34ed75b556388242198be6aef72
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-03 18:49:44 +0000

    Adds a null bite to the end of the array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -27,6 +27,7 @@ char *create_array(unsigned int size, char c)
 		array = (char *)malloc(size * sizeof(char));
 		for (i = 0; i < size; i++)
 			array[i] = c;
+		array[i] = '\0';
 	}
 
 	if (array == NULL)
