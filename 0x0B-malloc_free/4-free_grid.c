6b6dffa27b3d4842a3644937b8e08a4493502add
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-04 05:25:07 +0000

    First attempt at 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,16 +2,19 @@
 #include "holberton.h"
 
 /**
- * free_grid - a function to free up the memory from 
+ * free_grid - a function to free up the memory from
  * the previous exercise.
- * @grin: a double pointer leading to our grid
+ * @grid: a double pointer leading to our grid
  * @height: the height of our grid
  * Return: no return.
  */
 
 void free_grid(int **grid, int height)
 {
+	int x;
 
+	for (x = height; x >= 0; x--)
+		free(grid[x]);
 
-
+	free(grid);
 }
