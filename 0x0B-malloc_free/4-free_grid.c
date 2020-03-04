306ccc5feba3b477d4582b2590cabebf3e948a48
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-04 03:53:22 +0000

    Adds exercise 4 and 5

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * free_grid - a function to free up the memory from 
+ * the previous exercise.
+ * @grin: a double pointer leading to our grid
+ * @height: the height of our grid
+ * Return: no return.
+ */
+
+void free_grid(int **grid, int height)
+{
+
+
+
+}
---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * argostostr - a function to concatonate every argument
+ * sent to a function, and then print out the arguments
+ * one line at a time.
+ * @ac: the number of arguments
+ * @av: an array of arguments
+ * Return: a pointer to our new concatonated strings.
+ */
+
+char *argstostr(int ac, char **av)
+{
+
+
+
+}
