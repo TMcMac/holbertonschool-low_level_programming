11db4c362652c9ed32a1fb092ff8681ffc217466
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-05 22:06:32 +0000

    Adds exercise 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+int _strlen(char *s);
+/**
+ * string_nconcat - a function to concatonate a string with n bytes
+ * of a second string and return a pointer to the concatonation.
+ * @s1: a first string
+ * @s2: a second string to be concatonated with s1
+ * @n: an int to set number of byte copied from s2
+ * Return: a pointer to the concatonated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int j = 0;
+	unsigned int i = 0;
+	char *concat;
+	int len;
+	unsigned int s2len = _strlen(s2);
+
+/* Treat null value strings as empty string */
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	if (s2len > n)
+		s2len = n;
+	len = ((_strlen(s1) + 1) + (_strlen(s2) + 1));
+/* use malloc to pull space for concat  */
+	concat = malloc(len);
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++, j++)
+		concat[j] = s1[i];
+
+	for (i = 0; s2[i] != '\0' && i < s2len; i++, j++)
+		concat[j] = s2[i];
+
+	return (concat);
+}
+
+/**
+ * _strlen - a function to return the length of a string
+ *
+ * @s: a char pointer passed from main
+ *
+ * Return: returns an int for the length of s
+ */
+
+
+int _strlen(char *s)
+{
+	unsigned int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		continue;
+
+	return (len);
+}
