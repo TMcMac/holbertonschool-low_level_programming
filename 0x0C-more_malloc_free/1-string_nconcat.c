54aa6d41a0c3c05e32915e15313482c689cda3e9
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-05 22:14:38 +0000

    Fixes a betty style issue in the second for loop

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++, j++)
 		concat[j] = s1[i];
 
-	for (i = 0;i < s2len; i++, j++)
+	for (i = 0; i < s2len; i++, j++)
 		concat[j] = s2[i];
 	concat[j] = '\0';
 	return (concat);
