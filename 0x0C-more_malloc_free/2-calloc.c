cac80f70a9ca7a9c9a2732e08bdae13d541233ee
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-06 03:34:59 +0000

    Adds exercise 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - a function to make a array of nmemb # elements each
+ * of which is of size memory.
+ * @nmemb: the number of elements that will be in our array
+ * @size: the size in memory of each element in our array
+ * Return: return a void pointer to the first element in our array.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *array;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(nmemb * size);
+	if (array == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		*(array + i) = 0;
+	}
+
+	return (array);
+}
