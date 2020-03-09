9f323afed097dfecc69e83b4da0cda52ea6bc809
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-09 22:18:07 +0000

    Adds readme and exercise 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure for doggo and owner
+ * @name: the dog's name
+ * @age: age of the pupper
+ * @owner: the onwer's name
+ */
+
+extern int main(void);
+
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+};
+
+#endif
