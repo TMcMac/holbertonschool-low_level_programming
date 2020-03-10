20da5bce44784d84e7609185fd4a6a7d8ac2ae6b
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-10 04:00:32 +0000

    Adds strdup function to store Name and Owner info

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+
+char *_strdup(char *str);
 
 /**
  * new_dog - a function to list a new pupper's data
@@ -12,17 +15,62 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+	char *namestore;
+	char *ownerstore;
 
-	if (name == NULL || age == NULL || owner == NULL)
-		return;
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
 
-	dog_t->name = *name;
+	*namestore = _strdup(name);
+	*ownerstore = _strdup(owner);
+
+	dog_t->name = *namestore;
 	dog_t->age = age;
-	dog->owner = owner;
+	dog_t->owner = *ownerstore;
 
 	return (newdog);
+}
+
+/**
+ * _strdup - a function to copy a string to a new location and
+ * return a pointer to the new location.
+ *
+ * @str: an unsigned int fed from main that will be the size of our array
+ *
+ * Return: NULL or pointer to the filled array.
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int length = 0;
+	char *new;
+
+	if (str == '\0')
+	{
+		return (NULL);
+	}
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	new = (char *) malloc((length * sizeof(char)) + 1);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < length; i++)
+			new[i] = str[i];
+		new[i] = '\0';
+	}
+
+	return (new);
 }
