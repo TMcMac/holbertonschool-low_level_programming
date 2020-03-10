e75a0bf4f278f45ccac5cb7cef6a49be152ea120
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-10 02:28:28 +0000

    I had the pointer refs all wrong, changed everything to ->

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,18 +11,18 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		exit();
+		return;
 	}
 	else
 	{
-		if (d.name == NULL)
+		if (d->name == NULL)
 			d->name = "(nil)";
-		if (d.owner == NULL)
+		if (d->owner == NULL)
 			d->owner = "(nil)";
 
-		printf("Name: %s\n", d.name);
-		printf("Age: %f\n", d.age);
-		printf("Owner: %s\n", d.owner);
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 
 }
