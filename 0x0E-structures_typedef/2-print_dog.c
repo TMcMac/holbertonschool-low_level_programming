122a87ce1a3443399af134a25ae5967de6054016
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-10 02:20:26 +0000

    Adds exercise 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - a function to print a dogs details
+ * @d: a pointer to a struct of doggo info
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		exit();
+	}
+	else
+	{
+		if (d.name == NULL)
+			d->name = "(nil)";
+		if (d.owner == NULL)
+			d->owner = "(nil)";
+
+		printf("Name: %s\n", d.name);
+		printf("Age: %f\n", d.age);
+		printf("Owner: %s\n", d.owner);
+	}
+
+}
