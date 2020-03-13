a7f98364035402b015b657cba08df529fb3a0e7a
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-13 02:33:05 +0000

    Updates the main and header file for exercise 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,8 +1,16 @@
 #include "calc.h"
 
 /**
- *
- *
- *
+ * @op_add - a function to add two ints
+ * @a: a first int
+ * @b: a second int
+ * Return: the result of the addition.
  */
+
+int op_add(int a, int b)
+{
+	int result = a + b;
+	
+	return (result);
+}
 
