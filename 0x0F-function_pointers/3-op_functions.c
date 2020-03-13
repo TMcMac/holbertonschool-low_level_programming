338b0e8933f38cfae258ad7c527c5f23a712a532
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-13 03:24:19 +0000

    Updates mostly main

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,7 @@
 #include "calc.h"
 
 /**
- * @op_add - a function to add two ints
+ * op_add - a function to add two ints
  * @a: a first int
  * @b: a second int
  * Return: the result of the addition.
@@ -10,7 +10,62 @@
 int op_add(int a, int b)
 {
 	int result = a + b;
-	
+
 	return (result);
 }
 
+/**
+ * op_sub - a function to subtract and int from another
+ * @a: a first int
+ * @b: a second int
+ * Return: the result of the subtraction
+ */
+
+int op_sub(int a, int b)
+{
+	int result = a - b;
+
+	return (result);
+}
+
+/**
+ * op_mul - a function to multiply two ints
+ * @a: a first int
+ * @b: a second int
+ * Return: the result of the multiplication.
+ */
+
+int op_mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
+
+/**
+ * op_div - a function to divide one int by a second
+ * @a: a first int
+ * @b: a second int
+ * Return: the result of the division.
+ */
+
+int op_div(int a, int b)
+{
+	int result = a / b;
+
+	return (result);
+}
+
+/**
+ * op_mod - a function to modulus an int by another
+ * @a: a first int
+ * @b: a second int
+ * Return: the remainder after the division.
+ */
+
+int op_mod(int a, int b)
+{
+	int result = a % b;
+
+	return (result);
+}
