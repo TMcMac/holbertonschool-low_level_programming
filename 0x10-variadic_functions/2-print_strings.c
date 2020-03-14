bb8b039feb186aff11b34af3185715ddb995eb2a
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-14 01:40:27 +0000

    Adds exercise 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_strings - a function to print n number of strings
+ * @separator: a constant to be printed between stings
+ * @n: the number of arguments we will take in
+ * Return: Void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int count;
+
+	va_list(ap);
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+
+	for (count = 0; count < n; count++)
+	{
+		if (separator != NULL && count > 0)
+			printf("%s", separator);
+		if (va_arg(ap, char*) != NULL)
+			printf("%s", va_arg(ap, char*));
+		else
+			printf("(nil)");
+	}
+	printf("\n");
+	va_end(ap);
+}
