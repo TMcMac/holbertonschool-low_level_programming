8ee099558e34557c6877142bad48eb1309b08d25
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-19 21:24:44 +0000

    Adds exercsie 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node - a function that adds a node at the head
+ * of a linked list.
+ * @head: the pointer to the head of the list
+ * @str: a string to be copied into our new nodea
+ * Return: a pointer to our new ndoe
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head = malloc(sizeof(list_t));
+
+	if (new_head == NULL)
+		return (NULL);
+
+	new_head->str = strdup(str);
+	new_head->len = _strlen(str);
+	new_head->next = *head;
+	*head = new_head;
+
+	return (*head);
+}
+
+/**
+ * _strlen - a function to return the length of a string
+ * @s: a char pointer passed from main
+ * Return: returns an int
+ */
+
+int _strlen(const char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		continue;
+
+	return (len);
+}
