b7e690e35208340589d0775cb91bf10c089d38a3
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-24 18:19:20 +0000

    Meh

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,9 +17,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (newnode == NULL || head == NULL)
 		return (NULL);
+
 	tmp = (*head);
 	newnode->n = n;
 	newnode->next = NULL;
+
 	if (idx == 0)
 	{
 		newnode->next = (*head);
