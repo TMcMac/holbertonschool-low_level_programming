9abae2a16a7f3d6f8d1af460d8a6b95951abfa03
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-24 17:40:03 +0000

    Moves assignemnt to after the null check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,13 +12,20 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *tmp = (*head);
+	listint_t *tmp;
 	listint_t *newnode = malloc(sizeof(listint_t));
 
-	if (newnode == NULL || (*head) == NULL)
+	if (newnode == NULL || head == NULL)
 		return (NULL);
-
+	tmp = (*head);
 	newnode->n = n;
+	newnode->next = NULL;
+	if (idx == 0)
+	{
+		newnode->next = (*head);
+		(*head) = newnode;
+		return (newnode);
+	}
 
 	while (tmp != NULL && i < idx)
 	{
