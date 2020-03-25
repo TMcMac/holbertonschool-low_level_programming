aa28fc048d4368b15ff6b2d08e8174b3eb06146c
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-25 00:53:06 +0000

    Makes the while look idx -1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (newnode);
 	}
 	tmp = (*head);
-	while (i < idx)
+	while (i < idx - 1)
 	{
 		if (tmp == NULL)
 		{
