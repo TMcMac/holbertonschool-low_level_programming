23339f8b366d0553366a6f47dd87dda6b481c002
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-31 19:40:01 +0000

    First commit with set up files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - a function to read text from a file and
+ * write it to the POSIX stadout.
+ * @filename: a pointer to the file to be read
+ * @letters: the number of letters requested to be read
+ * Return: the number of letters actually read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd;
+	unsigned int num = letters + 1;
+	char *buf = malloc(num * sizeof(char));
+
+	if (buf == NULL)
+		return (-1);
+	if (filename == NULL || letters == 0)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == 0)
+	{
+		return (-1);
+	}
+
+	rd = read(fd, buf, letters);
+	buf[(rd + 1)] = '\0';
+	write(STDOUT_FILENO, buf, (rd + 1));
+	
+	close(fd);
+	free(buf);
+	return (rd);
+}
