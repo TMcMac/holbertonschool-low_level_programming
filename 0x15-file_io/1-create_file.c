416cffa5c387d8c8448fc2073d089f7092f987c7
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-04-01 02:23:29 +0000

    Adds O_TRUNC to the open

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int len = _strlen(text_content);
 
-	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT, O_TRUNC | S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (filename == NULL)
