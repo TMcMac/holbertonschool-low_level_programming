138353abd9f37ec2b1eebb686f04f5e2a7408ed7
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-04-01 03:16:13 +0000

    I was writing the file name to the file not text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,9 +26,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		len = _strlen(text_content);
 	else
-		return (-1);
+		return (1);
 
-	wrt = write(fd, filename, len);
+	wrt = write(fd, text_content, len);
 	if (wrt < 0)
 		return (-1);
 
