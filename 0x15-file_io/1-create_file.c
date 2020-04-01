e16da0e31958d5f642b07cea1ab826053c038ec2
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-04-01 02:32:42 +0000

    Moves the filename NULL fail condition up and adds an if for empty string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,18 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int len = _strlen(text_content);
 
-	fd = open(filename, O_WRONLY | O_CREAT, O_TRUNC | S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_WRONLY | O_CREAT, O_TRUNC | S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+		len = 1;
+	}
 	write(fd, filename, len);
 
 	close(fd);
