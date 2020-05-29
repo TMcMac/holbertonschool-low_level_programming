6805884c7ef3238e0015a4dce68e37e5951979db
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-05-29 23:59:57 +0000

    Adds null checks

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int position;
 	hash_node_t *tmp;
 
-	if (ht == NULL || key == NULL || key == "")
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (NULL);
 
 	position = key_index((unsigned char *)key, ht->size);
