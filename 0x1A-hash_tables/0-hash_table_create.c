0dedeb4bf96f1d749a34a2091a79eada15690414
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-05-29 20:34:14 +0000

    Uncomments suspect code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-/*	unsigned long int i; */
+	unsigned long int i;
 	/* first creat a new hash table */
 	hash_table_t *new_table = malloc(sizeof(hash_table_t) * 1);
 
@@ -25,10 +25,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_table);
 		return (NULL);
 	}
-/*	for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		// If we have be successful we'll set all array pointers to NULL
+		/* If we have be successful we'll set all array pointers to NULL */
 		new_table->array[i] = NULL;
 	}
-*/	return (new_table);
+	return (new_table);
 }
