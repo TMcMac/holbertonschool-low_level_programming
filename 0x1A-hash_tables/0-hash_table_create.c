2a4a0f955f1eb9c37256d9dd11df0d367ba171cc
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-05-29 20:23:45 +0000

    Adds exercsie 3

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,17 +9,26 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+	/* first creat a new hash table */
+	hash_table_t *new_table = malloc(sizeof(hash_table_t) * 1);
 
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	/* Now fill the array with pointers that can become nodes */
+	new_table->array = malloc(sizeof(hash_node_t*) * size);
 	if (new_table->array == NULL)
 	{
+		/* If it fails we need to free our hashtable */
 		free(new_table);
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		/* If we have be successful we'll set all array pointers to NULL */
+		new_table->array[i] = NULL;
 	}
 	return (new_table);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - a function to take in a hash table,
+ * get the hash of a key which is then stored at the
+ * position of key mod table size.
+ * @ht: the hash table built by main/0-hash_table
+ * @key: the key string to be hashed by djb2
+ * @value: the value to be stored at the position
+ * Return: 1 for success or 0 for failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int size = ht->size;
+	unsigned long int position = key_index((unsigned char *)key, size);
+	hash_node_t *node, *tmp;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+
+	if (ht->array[position] == NULL)
+	{
+		node = malloc(sizeof(hash_node_t) * 1);
+		node->key = malloc(strlen(key) + 1);
+		node->value = malloc(strlen(value) + 1);
+		strcpy(node->key, key);
+		strcpy(node->value, value);
+		node->next = NULL;
+		ht->array[position] = node;
+	}
+	else if (strcmp((ht->array[position]->key), key) == 0)
+		ht->array[position]->value = (char *)value;
+	else
+	{
+		tmp = ht->array[position];
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		node = malloc(sizeof(hash_node_t) * 1);
+		node->key = malloc(strlen(key) + 1);
+		node->value = malloc(strlen(value) + 1);
+		strcpy(node->key, key);
+		strcpy(node->value, value);
+		node->next = NULL;
+		tmp->next = node;
+	}
+
+	return (1);
+}
