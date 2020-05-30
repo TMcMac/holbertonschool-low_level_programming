c2a1f045d8bfd5128afdf4456f9077c9781bb396
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-05-30 00:38:28 +0000

    Adds exercise 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - a function to cycle through a hashtable
+ * and print all valid key:value pairs
+ * @ht: a hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int punct = 0;
+
+	if (ht == NULL)
+		exit(0);
+	else
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i] != NULL)
+			{
+				if (punct)
+					printf(", ");
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				punct = 1;
+			}
+		}
+		printf("}\n");
+	}
+}
