69d26ea175bed2307bc79b7e55dbc58bfc41454f
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-07-08 03:16:40 +0000

    Updates 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -21,18 +21,17 @@ def island_perimeter(grid):
             if grid[i][j] == 1 and grid[i][j-1] != 1:
                 """ If you are on land and land is not behind you """
                 perimeter += 1
-            if grid[i][j] == 1 and grid[i-1][j] == 0:
-                """ If you are on land and water IS above you """
+            if grid[i][j] == 1 and (grid[i-1][j] == 0 or i == 0):
+                """ If you are on land and water or the edge IS above you """
                 perimeter += 1
-            if grid[i][j] == 1 and (grid[i-1][j] is None or
-                                    grid[i+1][j] is None):
-                """ If you are on land and on the edge of the map """
+            if grid[i][j] == 1 and (i + 1) == rows:
+                """ If you are on land and on the right edge of the map """
                 perimeter += 1
             if grid[i][j] == 0 and grid[i][j-1] == 1:
                 """ If you are on water but land IS behind you """
                 perimeter += 1
-            if grid[i][j] == 0 and grid[i-1][j] == 1:
-                """ If you are on water bu land is above you """
+            if grid[i][j] == 0 and (grid[i-1][j] == 1 and i != 0):
+                """ If you are on water but land is above you """
                 perimeter += 1
 
     return perimeter
