1ce93980594c116b4d6fc371dbb4ade39c351c2c
Tim <timmcmackenjr@gmail.com>
2020-09-06 19:34:05 -0500

    Removes moving pointer and uses head directly

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,9 +8,10 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *mover;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
+	if (*new_node == NULL)
+		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
 
@@ -18,11 +19,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		*head = new_node;
 		new_node->next = NULL;
-		return (new_node);
 	}
-	mover = *head;
-	new_node->next = mover;
-	mover->prev = new_node;
+	else
+	{
+	new_node->next = *head;
+	*head->prev = new_node;
 	*head = new_node;
+	}
 	return (new_node);
 }
