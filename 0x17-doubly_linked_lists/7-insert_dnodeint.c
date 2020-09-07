d2e587c0072b6e9985210ca97aae14770be9f3fd
Tim <timmcmackenjr@gmail.com>
2020-09-07 09:58:39 -0500

    Fixes position 0 failure

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,11 +15,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	dlistint_t *position_node = get_dnodeint_at_index(*h, idx);
 
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		exit(1);
+	else
+	{
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+	}
+
+	if (idx == 0)
+	{
+		new_node->next = (*h);
+		(*h) = new_node;
+		return (new_node);
+	}
 	if (position_node == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(dlistint_t));
-	new_node->n = n;
 	new_node->next = position_node;
 	new_node->prev = position_node->prev;
 	position_node->prev->next = new_node;
@@ -40,8 +54,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *list, unsigned int index)
 	unsigned int count = 0;
 	dlistint_t *mover;
 
-	if (list == NULL)
-		return (NULL);
 	mover = list;
 	while (count < index && mover != NULL)
 	{
