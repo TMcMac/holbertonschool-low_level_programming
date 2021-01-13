877f4b1a3a8f701d717d2770bb6beefc9db5ea64
TMcMac <timmcmackenjr@gmail.com>
2021-01-13 22:20:34 +0000

    1 is sort of working but my windows are wrong and it needs formating

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,24 +1,33 @@
 #include "search_algos.h"
 /**
- *
- *
+ * binary_search - a program to serach through an array
+ * for a specified value and return the index if found
+ * @array: a pointer to position zero
+ * @size: the unsigned int size of the array
+ * @value: the value we are searching for
+ * Return: the index position if found, -1 if not found
  */
 
 
 int binary_search(int *array, size_t size, int value)
 {
-  unsigned int mid, min, max;
+  unsigned int i, j, idx;
 
   if (array == NULL || size <= 0)
-    {
       return (-1);
-    }
 
   i = (size / 2);
+  printf("Searching in array: ");
+  for (j = 0; j < (size - 1); j++)
+    printf("%d, ", array[j]);
+  printf("%d\n", array[j]);
   if (array[i] == value)
-    {
-      
-    }
+    return (i);
+  if (array[i] > value)
+    idx = binary_search(array, i, value);
+  if (array[i] < value)
+    idx = binary_search(&(array[i]), i, value);
   
+  return(idx);
 }
 
