74177e836ba8b83481e9ead7154850b7da0f9d28
TMcMac <timmcmackenjr@gmail.com>
2021-01-13 03:11:22 +0000

    Initial commit

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+/**
+ *
+ *
+ */
+
+
+int binary_search(int *array, size_t size, int value)
+{
+  unsigned int mid, min, max;
+
+  if (array == NULL || size <= 0)
+    {
+      return (-1);
+    }
+
+  i = (size / 2);
+
+  
+}
+
